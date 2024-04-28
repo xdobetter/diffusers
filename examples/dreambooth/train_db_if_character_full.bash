7ac3740bf949aa89147febb7b14a4b3bc2440aca
DoBetter <db.xi@zju.edu.cn>
2024-04-28 10:46:21 +0800

    finish dreambooth dev

--- examples/dreambooth/train_db_if_character_full.bash
+++ examples/dreambooth/train_db_if_character_full.bash
@@ -1,6 +1,5 @@
 set -v
 
-
 # s1_girl_character_v2
 export project_name="s1_girl_character_if_v1" # !modify
 export MODEL_NAME="DeepFloyd/IF-I-XL-v1.0"
@@ -10,7 +9,6 @@ export dataset="character" # !modify
 export INSTANCE_DIR="data/$dataset/$INSTANCE_ID"
 export LOGGING_DIR="log/$project_name/$INSTANCE_ID"
 export CLASS_DIR="ppl/anime_characters" # !modify
-
 
 accelerate launch train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
@@ -27,7 +25,6 @@ accelerate launch train_dreambooth.py \
   --validation_steps=50 \
   --validation_prompt="a sks female anime character waving hand  with a tree and autumn leaves in the background" \
   --logging_dir=$LOGGING_DIR \
-  --enable_xformers_memory_efficient_attention \
   --checkpointing_steps=500 \
   --with_prior_preservation --prior_loss_weight=1.0 \
   --class_data_dir=$CLASS_DIR \
@@ -35,75 +32,8 @@ accelerate launch train_dreambooth.py \
   --num_class_images=1000 \
   --num_validation_images=16 \
   --pre_compute_text_embeddings \
-  --tokenizer_max_length=77
-
-
-# # s2_girl_character_debug
-# export project_name="s2_girl_character_debug"
-# export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-# export INSTANCE_ID="s2_girl_character"
-# export OUTPUT_DIR="model/$project_name"
-# export dataset="character"
-# export INSTANCE_DIR="data/$dataset/$INSTANCE_ID"
-# export LOGGING_DIR="log/$project_name/$INSTANCE_ID"
-# export CLASS_DIR="ppl/anime_characters"
-
-
-# accelerate launch train_dreambooth.py \
-#   --pretrained_model_name_or_path=$MODEL_NAME  \
-#   --instance_data_dir=$INSTANCE_DIR \
-#   --output_dir=$OUTPUT_DIR \
-#   --instance_prompt="a sks female anime character" \
-#   --resolution=512 \
-#   --train_batch_size=1 \
-#   --gradient_accumulation_steps=1 \
-#   --learning_rate=1e-6 \
-#   --lr_scheduler="constant" \
-#   --lr_warmup_steps=0 \
-#   --max_train_steps=2000 \
-#   --validation_steps=50 \
-#   --validation_prompt="a sks female anime character waving hand  with a tree and autumn leaves in the background" \
-#   --logging_dir=$LOGGING_DIR \
-#   --enable_xformers_memory_efficient_attention \
-#   --checkpointing_steps=500 \
-#   --with_prior_preservation --prior_loss_weight=1.0 \
-#   --class_data_dir=$CLASS_DIR \
-#   --class_prompt="a female anime character" \
-#   --num_class_images=1000 \
-#   --num_validation_images=16 \
-#   --train_text_encoder
-
-# # s10_girl_character_v1
-# export project_name="s10_girl_character_v1"
-# export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-# export INSTANCE_ID="s10_girl_character"
-# export OUTPUT_DIR="model/$project_name"
-# export dataset="character"
-# export INSTANCE_DIR="data/$dataset/$INSTANCE_ID"
-# export LOGGING_DIR="log/$project_name/$INSTANCE_ID"
-# export CLASS_DIR="ppl/anime_characters"
-
-
-# accelerate launch train_dreambooth.py \
-#   --pretrained_model_name_or_path=$MODEL_NAME  \
-#   --instance_data_dir=$INSTANCE_DIR \
-#   --output_dir=$OUTPUT_DIR \
-#   --instance_prompt="a sks female anime character" \
-#   --resolution=512 \
-#   --train_batch_size=1 \
-#   --gradient_accumulation_steps=1 \
-#   --learning_rate=1e-6 \
-#   --lr_scheduler="constant" \
-#   --lr_warmup_steps=0 \
-#   --max_train_steps=2000 \
-#   --validation_steps=50 \
-#   --validation_prompt="a sks female anime character waving hand  with a tree and autumn leaves in the background" \
-#   --logging_dir=$LOGGING_DIR \
-#   --enable_xformers_memory_efficient_attention \
-#   --checkpointing_steps=500 \
-#   --with_prior_preservation --prior_loss_weight=1.0 \
-#   --class_data_dir=$CLASS_DIR \
-#   --class_prompt="a female anime character" \
-#   --num_class_images=1000 \
-#   --num_validation_images=16 \
-#   --train_text_encoder
+  --tokenizer_max_length=77 \
+  --enable_xformers_memory_efficient_attention \
+  --gradient_checkpointing \
+  --use_8bit_adam \
+  --mixed_precision="fp16" 
